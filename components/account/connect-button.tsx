8618e0ffbf1ea5f261a9fb6834a860234f5161f9
theNvN <thenvnsahu@gmail.com>
2022-03-13 00:51:40 +0530

    show account info modal if connected

--- components/account/connect-button.tsx
+++ components/account/connect-button.tsx
@@ -1,17 +1,22 @@
 import { Button, ButtonProps } from '@chakra-ui/react';
+import { useUI } from 'contexts/ui';
 import { useWeb3Provider } from 'contexts/web3-provider';
 
 const ConnectWalletButton: React.FC<ButtonProps> = ({ ...props }) => {
-  const { connect, disconnect, isLoading, account, isConnected } = useWeb3Provider();
+  const { connect, isLoading, account } = useWeb3Provider();
+  const { setModalViewAndOpen } = useUI();
 
   function handleClick() {
-    if (!isConnected) connect();
-    else disconnect();
+    if (!account.isConnected) connect();
+    else {
+      setModalViewAndOpen('ACCOUNT_INFO');
+    }
   }
 
   let btnLabel = 'Connect Wallet';
   if (isLoading) btnLabel = 'Connecting...';
-  else if (isConnected) btnLabel = `Disconnect ${account?.address?.slice(0, 8).concat('...')}`;
+  else if (account.isConnected)
+    btnLabel = `Connected ${account?.address?.slice(0, 8).concat('...')}`;
 
   return (
     <Button onClick={handleClick} isLoading={isLoading} {...props}>
