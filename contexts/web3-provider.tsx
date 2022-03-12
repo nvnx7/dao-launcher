5655da8b108e5bc6fdd7be0a645c03648fccb79b
theNvN <thenvnsahu@gmail.com>
2022-03-13 00:52:06 +0530

    refactored

--- contexts/web3-provider.tsx
+++ contexts/web3-provider.tsx
@@ -1,68 +1,103 @@
-import { createContext, useContext, useMemo, useState } from 'react';
-import { ethers } from 'ethers';
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { BigNumber, ethers } from 'ethers';
 import Web3Modal from 'web3modal';
+import { useRouter } from 'next/router';
+import logger from 'utils/logger';
+import { Network } from '@ethersproject/networks';
+
+interface Account {
+  isConnected: boolean;
+  address?: string;
+  balance?: BigNumber;
+}
 
 interface State {
   provider?: ethers.providers.Web3Provider;
-  isConnected: boolean;
+  account: Account;
+  network?: Network;
+  isLoading: boolean;
 }
 
-const initialState: State = { isConnected: false, provider: undefined };
-
+const initialState: State = { account: { isConnected: false }, isLoading: false };
 export const Web3ProviderContext = createContext<State | any>(initialState);
-
 Web3ProviderContext.displayName = 'Web3ProviderContext';
 
+let web3Modal: Web3Modal;
 export const Web3Provider: React.FC = (props) => {
-  const [isConnected, setConnected] = useState<boolean>(false);
-  const [isLoading, setLoading] = useState(false);
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider | undefined>(undefined);
-  const [account, setAccount] = useState<object>();
-  const [web3Modal, setWeb3Modal] = useState<Web3Modal | undefined>(undefined);
+  const router = useRouter();
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
+  const [account, setAccount] = useState<Account>(initialState.account);
+  const [network, setNetwork] = useState<Network>();
 
-  async function connect() {
-    setLoading(true);
-    const modal = new Web3Modal({ cacheProvider: true });
-    setWeb3Modal(modal);
-    await modal
-      .connect()
-      .then((conn) => {
-        const provider = new ethers.providers.Web3Provider(conn);
-        provider.on('disconnect', (error) => {
-          console.log('Provider disconnected', error);
-          setConnected(false);
-        });
+  useEffect(() => {
+    if (!provider) return;
+    const signer = provider.getSigner();
 
-        setProvider(provider);
-        setConnected(true);
-
-        return provider.getSigner().getAddress();
+    // get account and network data
+    Promise.all([signer.getAddress(), signer.getBalance(), provider.getNetwork()])
+      .then(([address, balance, network]) => {
+        logger.log({ address, balance, network });
+        setAccount({ address, balance, isConnected: true });
+        setNetwork(network);
       })
-      .then((address) => setAccount({ address }))
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  }
+      .catch(logger.error);
+  }, [provider]);
 
-  function disconnect() {
+  const disconnect = useCallback(() => {
     web3Modal?.clearCachedProvider();
-    setConnected(false);
-    setAccount({});
+    provider?.removeAllListeners();
+    setAccount({ isConnected: false });
+    setNetwork(undefined);
     setProvider(undefined);
-    setWeb3Modal(undefined);
-  }
+    router.reload();
+  }, [provider, router]);
+
+  const connect = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      web3Modal = new Web3Modal({ cacheProvider: true });
+      const instance = await web3Modal.connect();
+
+      instance.on('connect', (info: any) => {
+        logger.log('connected!', info);
+      });
+
+      instance.on('chainChanged', (chainId: number) => {
+        logger.log(`chain changed to ${chainId}! updating providers`);
+        setProvider(new ethers.providers.Web3Provider(instance));
+      });
+
+      instance.on('accountsChanged', () => {
+        logger.log(`account changed!`);
+        setProvider(new ethers.providers.Web3Provider(instance));
+      });
+
+      instance.on('disconnect', (code: number, reason: string) => {
+        logger.log(code, reason);
+        disconnect();
+      });
+
+      setProvider(new ethers.providers.Web3Provider(instance));
+      setLoading(false);
+    } catch (error) {
+      logger.error(error);
+      setLoading(false);
+    }
+  }, [setProvider, disconnect]);
 
   const value = useMemo<State>(
     () => ({
       isLoading,
-      web3Modal,
       provider,
       connect,
       disconnect,
-      isConnected,
       account,
+      network,
     }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [isLoading, web3Modal, provider, isConnected, account],
+    [isLoading, web3Modal, provider, account, network],
   );
 
   return <Web3ProviderContext.Provider value={value} {...props} />;
