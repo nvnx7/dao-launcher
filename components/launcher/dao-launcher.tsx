e31b02ba7739d8d90ff230d62cfd7d2299a05108
theNvN <thenvnsahu@gmail.com>
2022-03-13 19:00:19 +0530

    refactored launcher to show dao info

--- components/launcher/dao-launcher.tsx
+++ components/launcher/dao-launcher.tsx
@@ -1,20 +1,23 @@
+import { useEffect } from 'react';
 import {
   Box,
   Button,
   Heading,
   HStack,
-  ListItem,
   Spinner,
   StackProps,
   Text,
-  UnorderedList,
+  useToast,
   VStack,
 } from '@chakra-ui/react';
-import useDaoLauncher from 'hooks/useDaoLauncher';
+import useDaoLauncher, { DaoLauncherParams } from 'hooks/useDaoLauncher';
 import { CheckIcon } from '@chakra-ui/icons';
 import { useUI } from 'contexts/ui';
+import DaoInfo from 'components/dao/dao-info';
 
-interface IDaoLauncherProps extends StackProps {}
+interface IDaoLauncherProps extends StackProps {
+  params: DaoLauncherParams;
+}
 
 const LaunchStep: React.FC<{ isDone: boolean; label: string }> = ({ isDone, label }) => {
   return (
@@ -25,9 +28,27 @@ const LaunchStep: React.FC<{ isDone: boolean; label: string }> = ({ isDone, labe
   );
 };
 
-const DaoLauncher: React.FC<IDaoLauncherProps> = ({ ...props }) => {
-  const { modalData: params } = useUI();
-  const { deployDao, isLoading, status, transactions } = useDaoLauncher();
+const DaoLauncher: React.FC<IDaoLauncherProps> = ({ params, ...props }) => {
+  const toast = useToast();
+  const { setModalData, setModalViewAndOpen } = useUI();
+  const { deployDao, isLoading, status, transactions, isSuccessful, isError } = useDaoLauncher();
+
+  useEffect(() => {
+    if (isError) {
+      toast({ status: 'error', title: 'Error!', description: 'Something went wrong :(' });
+    }
+    if (!isSuccessful) return;
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccessful, isError]);
+
+  useEffect(() => {
+    if (transactions) {
+      setModalData(transactions);
+      setModalViewAndOpen('DEPLOYMENT_DATA_DOWNLOADER');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [transactions]);
 
   const handleLaunch = () => {
     deployDao(params);
@@ -36,20 +57,18 @@ const DaoLauncher: React.FC<IDaoLauncherProps> = ({ ...props }) => {
   return (
     <VStack alignItems="center" spacing={8} {...props}>
       <Heading color="primary.500" alignSelf="center">
-        Launcher🚀
+        Launch
       </Heading>
       {isLoading ? (
         <Box>
-          <LaunchStep isDone={status.tokenDeployed} label="Deploying Token Contract" />
-          <LaunchStep isDone={status.timelockDeployed} label="Deploying Timelock Contract" />
-          <LaunchStep isDone={status.governorDeployed} label="Deploying Governor Contract" />
+          <LaunchStep isDone={status.isTokenDeployed} label="Deploying Token Contract" />
+          <LaunchStep isDone={status.isTimelockDeployed} label="Deploying Timelock Contract" />
+          <LaunchStep isDone={status.isGovernorDeployed} label="Deploying Governor Contract" />
         </Box>
       ) : (
-        <UnorderedList>
-          <ListItem>Deploying Token Contract</ListItem>
-          <ListItem>Deploying Timelock Contract</ListItem>
-          <ListItem>Deploying Governor Contract</ListItem>
-        </UnorderedList>
+        <Box w="full">
+          <DaoInfo params={params} />
+        </Box>
       )}
 
       <Button mb={4} isLoading={isLoading} onClick={handleLaunch} alignSelf="center">
