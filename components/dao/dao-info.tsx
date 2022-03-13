ffed61905d73f5c92e65aabfc6dbc2ac29d50ff6
theNvN <thenvnsahu@gmail.com>
2022-03-13 18:58:34 +0530

    display params in human redable units

--- components/dao/dao-info.tsx
+++ components/dao/dao-info.tsx
@@ -1,38 +1,65 @@
 import { Heading, VStack, Text, StackProps, Box } from '@chakra-ui/react';
 import LabelValuePair from 'components/common/label-value';
 import { DaoLauncherParams } from 'hooks/useDaoLauncher';
+import { blocksToDays, blocksToHours, secondsToHours } from 'utils/date-time';
 
 interface IDaoInfoProps extends StackProps {
   params: DaoLauncherParams;
 }
-
+const blockTime = 13.6; //sec
 const DaoInfo: React.FC<IDaoInfoProps> = ({ params, ...props }) => {
   const { dao, token, timelock, governor } = params;
   return (
-    <VStack alignItems="stretch" {...props}>
-      <Heading>{dao.name}</Heading>
-      <Box>
+    <VStack w="full" {...props}>
+      <Heading fontSize="2xl" color="primary.500">
+        {dao.name}
+      </Heading>
+      <Box w="full">
         <Text color="primary.500" fontWeight="bold">
           Token
         </Text>
-        <LabelValuePair label="Type" value={token.type?.toUpperCase()} />
-        <LabelValuePair label="Name" value={token.name} />
-        <LabelValuePair label="Symbol" value={token.symbol} />
+        {token?.haveAddress ? (
+          <LabelValuePair label="Address" value={token.address} />
+        ) : (
+          <>
+            <LabelValuePair label="Type" value={token.type?.toUpperCase()} />
+            <LabelValuePair label="Name" value={token.name} />
+            <LabelValuePair label="Symbol" value={token.symbol} />
+            <LabelValuePair label="Initial Supply" value={`${token.initialSupply} tokens`} />
+          </>
+        )}
       </Box>
 
-      <Box>
+      <Box w="full">
         <Text fontWeight="bold" color="primary.500">
           Timelock
         </Text>
-        <LabelValuePair label="Delay" value={`${timelock.minDelay} hour(s)`} />
+        {timelock?.haveAddress ? (
+          <LabelValuePair label="Address" value={`${timelock.address}`} />
+        ) : (
+          <LabelValuePair
+            label="Delay"
+            value={`${timelock.minDelay} sec (~${secondsToHours(timelock.minDelay)} hours)`}
+          />
+        )}
       </Box>
 
-      <Box>
+      <Box w="full">
         <Text fontWeight="bold" color="primary.500">
           Governor
         </Text>
-        <LabelValuePair label="Voting Delay" value={`${governor.votingDelay} hour(s)`} />
-        <LabelValuePair label="Voting Period" value={`${governor.votingPeriod} day(s)`} />
+        <LabelValuePair
+          label="Voting Delay"
+          value={`${governor.votingDelay} blocks (~${blocksToHours(blockTime)(
+            governor.votingDelay,
+          )} hours)`}
+        />
+        <LabelValuePair
+          label="Voting Period"
+          value={`${governor.votingPeriod} blocks (~${blocksToDays(blockTime)(
+            governor.votingPeriod,
+          )} days)`}
+        />
         <LabelValuePair label="Proposal Threshold" value={`${governor.proposalThreshold} tokens`} />
         <LabelValuePair label="Quorum Fraction" value={`${governor.quorumFraction}%`} />
       </Box>
